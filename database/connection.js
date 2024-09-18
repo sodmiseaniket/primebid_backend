b01c689a2a53dc794fee656bb90fc93228584329
Aniket Bapu Sodmise <65647514+sodmiseaniket@users.noreply.github.com>
2024-09-18 14:36:22 +0530

    Update connection.js

--- database/connection.js
+++ database/connection.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
  export const connection = async() => {
-    await mongoose.connect('mongodb+srv://g22aniketsodmise:niket1234@cluster0.epzvb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',{
+    await mongoose.connect('mongodb+srv://g22aniketsodmise:niket1234@cluster0.epzvb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&tls=true',{
       dbName: "MERN_AUCTION_PLATFORM"
     }).then(()=>console.log("DB Connected"));
  }
