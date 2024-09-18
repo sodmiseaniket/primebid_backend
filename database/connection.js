815f71a41bf5002141111f5be5ffb9fef7e48719
Aniket Bapu Sodmise <sodmiseaniket2004@gmail.com>
2024-09-18 10:02:01 +0530

    commit code

---
+++ database/connection.js
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+
+ export const connection = async() => {
+    await mongoose.connect('mongodb+srv://g22aniketsodmise:niket1234@cluster0.epzvb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',{
+      dbName: "MERN_AUCTION_PLATFORM"
+    }).then(()=>console.log("DB Connected"));
+ }
+ 
